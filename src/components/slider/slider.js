daeac7d181ea4da09cc9322bed0bef8301939f71
Beka0530 <0555991093b@gmail.com>
2022-03-16 04:24:52 +0700

    you dont find my file

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -11,25 +11,25 @@ const Slider = () => {
     return (
     <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="4000">
+            <div class="carousel-item active" data-bs-interval="2000">
                 <img src={bank1} class="d-block w-100" alt="..."/>
             </div>
-            <div class="carousel-item" data-bs-interval="4000">
+            <div class="carousel-item" >
                 <img src={bank2} class="d-block w-100" alt="..."/>
             </div>
-            <div class="carousel-item" data-bs-interval="4000">
+            <div class="carousel-item" >
                 <img src={bank3} class="d-block w-100" alt="..."/>
             </div>
-            <div class="carousel-item" data-bs-interval="4000">
+            <div class="carousel-item" >
                 <img src={bank4} class="d-block w-100" alt="..."/>
             </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
-            <span><img className="leftArrow" src={left} /></span>
+            <span class="carousel-control-prev-icon" aria-hidden="true"><img className="leftArrow" src={left} /></span>
             <span class="visually-hidden">Previous</span>
         </button>
         <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
-            <span><img className="rightArrow" src={right} /></span>
+            <span class="carousel-control-next-icon" aria-hidden="true"><img className="rightArrow" src={right} /></span>
             <span class="visually-hidden">Next</span>
         </button>
     </div>
